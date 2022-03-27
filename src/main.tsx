2af0a244bd4c102b4759f7d60ef3a1994dd562d0
Balaon <balaev_jura@mail.ru>
2022-03-27 16:10:00 +0300

    Init

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1 className="text-3xl font-bold underline"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, porro ipsa! Tenetur labore repellendus ut alias, dolorum voluptatibus est quaerat magni corrupti minima? </h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { join, resolve } from 'node:path';
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@app': resolve(join(__dirname, 'src', 'app')),
+    },
+  },
+})
